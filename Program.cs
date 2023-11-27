cac9eea9d4e602240044fa2aa7871943c816d823
Lex Klaassen <kllex028@gmail.com>
2023-11-27 13:48:06 +0100

    gebruik van dictionaries

--- Program.cs
+++ Program.cs
@@ -1,51 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace GroteOpdracht
 {
     class Program
     {
         int tellertje = 0;
+        public Dictionary<(int, int), (int, int)> afstandenDict;
+
         static void Main()
         {
             // bedrijvenlijst:
-            Bedrijf[] bedrijvenlijst = new Bedrijf[10];
-            LinkedList<Bedrijf> list = new LinkedList<Bedrijf>();
+            Dictionary<int, Bedrijf> grabbelton = new Dictionary<int, Bedrijf>();
             using (var reader = new StreamReader("../../../orderbestand.txt"))
             {
                 string firstline = reader.ReadLine();
                 Console.WriteLine($"{firstline}");
+
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
                     string[] parts = line.Split(';');
-                    list.AddFirst(new Bedrijf(parts));
+                    Bedrijf bv = new Bedrijf(parts);
+                    grabbelton.Add(bv.matrixID, bv);
                 }
             }
-            Console.WriteLine(list.First.Value.order);
+            Console.WriteLine(grabbelton[0]);
 
-            // get the distance matrix
-            int len = 0;
-            foreach (var bedrijf in list)
-            {
-                if (bedrijf.matrixID > len) { len = bedrijf.matrixID; }
-            }
-            Console.WriteLine($"{len}");
-            MatrixVak[,] matrix = new MatrixVak[len+1,len+1];
+
+            Dictionary<(int, int), (int, int)> dict = new Dictionary<(int, int), (int, int)>();
             using (var reader = new StreamReader("../../../afstandenmatrix.txt"))
             {
-                string firstline = reader.ReadLine() ;
+                string firstline = reader.ReadLine();
                 string line;
                 while (( line = reader.ReadLine()) != null )
                 {
                     string[] parts = line.Split(";");
                     int id1 = int.Parse(parts[0]);
                     int id2 = int.Parse(parts[1]);
-                    matrix[id1, id2] = new MatrixVak(parts);
+                    int afstand = int.Parse(parts[2]); // eigenlijk niet nodig ivm de rijtijd
+                    int tijd = int.Parse(parts[3]);
+                    dict[(id1, id2)] = (afstand, tijd);
                 }
             }
-            Console.WriteLine(matrix[0,1].afstand);
+            Console.WriteLine(dict[(0,1)].Item1);
         }
 
         static Oplossing ILS()
@@ -53,41 +53,70 @@ namespace GroteOpdracht
             Oplossing oplossing = new Oplossing();
             return oplossing;
         }
-        static void Operation()
+        static void swapOperation()
         {
-            // check de buurruimte dmv add and remove
-            // je kan werken met een enkele truck die twee keer zo lang mag rijden,
-            // 200.000L capaciteit <- is dat nog hetzelfde dan?
 
         }
-        static float kosten(Route r)
+        void addBv(Route r, Bedrijf predecessor, Bedrijf b)
         {
-            return 0;
-        }
-        static bool isValidRoute(Route r, MatrixVak[,] matrix)
-        {
-            // is de rijtijd + leegtijd binnen een dag
-            // eindigt de route bij id 287
+            int nextIndex = afstandenDict.Count + 1;
+            b.predecessor = predecessor;
+            b.successor = predecessor.successor;
 
-            float tijdspanne = 0; // minuten
-            float truckvolume = 0;
-            LinkedListNode<Bedrijf> node = r.route.First;
-            while (node.Next != null)
+            predecessor.successor.predecessor = b;
+            predecessor.successor = b;
+
+            b.rtNaar = afstandenDict[(b.predecessor.matrixID, b.matrixID)].Item2; // kan dit niet beter bij het uitrekenen van de increment?
+            b.rtVan = afstandenDict[(b.matrixID, b.successor.matrixID)].Item2;
+            
+            r.route.Add(nextIndex, b);
+        }
+        public static void removeBv(Route r, int key, Bedrijf b)
+        {
+            r.route.Remove(key); // O(1)
+            b.predecessor.successor = b.successor;
+            b.successor.predecessor = b.predecessor;
+        }
+        float beginkosten(Route r, Dictionary<int, Bedrijf> grabbelton)
+        {
+            float routekosten = 0;
+            float grabbeltonKosten = 0;
+            for (int i = 0; i < grabbelton.Count; i++)
             {
-                if (node.Value.matrixID == 287)
+                grabbeltonKosten += grabbelton[i].ldm * 3;
+            }
+            for (int i = 0; i < r.route.Count; i++)
+            {
+                if (r.route[i].predecessor != null)
                 {
-                    // leeg een truck binnen 30 minuten
-                    tijdspanne += 30;
-                    truckvolume = 0;
-                } else
-                {
-                    tijdspanne += matrix[node.Value.matrixID, node.Next.Value.matrixID].rijtijd + node.Value.ldm;
-                    truckvolume += node.Value.cont * node.Value.vpc;
+                    routekosten += afstandenDict[(r.route[i].predecessor.matrixID, r.route[i].matrixID)].Item2;
                 }
             }
-            if (tijdspanne > 11.50 * 2 * 60) { return false; }
-            if (r.route.Last.Value.matrixID != 287) { return false; }
-            if (truckvolume > 200000) { return false; }
+            return routekosten + grabbeltonKosten;
+        }
+
+        // dit is de increment voor de add functie
+        static float increment(Bedrijf b)
+        {
+            // hangt af van de operation
+            // als je een bedrijf toevoegd aan de route:
+            float inc = -(b.ldm * 3) + b.rtVan + b.rtNaar;
+            return inc;
+        }
+        public bool isValidRoute(Route r, Bedrijf b)
+        {
+            if (b.matrixID == 287)
+            {
+                // leeg een truck binnen 30 minuten
+                r.tijdsduur += 30 + afstandenDict[(b.predecessor.matrixID, b.matrixID)].Item2;
+                r.capacitiet = 0;
+            } else
+            {
+                r.tijdsduur += afstandenDict[(b.predecessor.matrixID, b.matrixID)].Item2 + b.ldm;
+                r.capacitiet += b.cont * b.vpc;
+            }
+            if (r.tijdsduur > 11.50 * 2 * 60) { return false; }
+            if (r.capacitiet > 200000) { return false; }
             return true;
         }
     }
@@ -101,41 +130,33 @@ namespace GroteOpdracht
     }
     public class Route
     {
-        public LinkedList<Bedrijf> route;
-        public Route() { }
+        public Dictionary<int, Bedrijf> route; // key is een index
+        public float tijdsduur;
+        public float capacitiet;
+        // willen we een laatste elementen bijhouden.
+        public Route() 
+        {
+            route = new Dictionary<int, Bedrijf>(); // is dit nodig
+        }
     }
     public class Bedrijf
     {
-        public int order; // niet nodig toch?
-        public string plaats; // niet nodig
+        public Bedrijf successor;
+        public Bedrijf predecessor; // nodig voor als je een bedrijf weghaalt uit je route
         public string freq;
         public int cont;
         public int vpc;
         public float ldm;
         public int matrixID;
-        public int xcoord; // niet nodig
-        public int ycoord; // niet nodig
+        public int rtVan; // rijtijd vanaf dit bedrijf naar zijn successor
+        public int rtNaar; // rijtijd naar dit bedrijf vanaf zijn predecessor
         public Bedrijf(string[] parts)
         {
-            order = int.Parse(parts[0]);
-            plaats = parts[1];
             freq = parts[2];
             cont = int.Parse(parts[3]);
             vpc = int.Parse(parts[4]);
             ldm = float.Parse(parts[5]);
             matrixID = int.Parse(parts[6]);
-            xcoord = int.Parse(parts[7]);
-            ycoord = int.Parse(parts[8]);// hello world bye bgusysd fee ree buz fuz foo bar
-        }
-    }
-    public class MatrixVak
-    {
-        public int afstand;
-        public int rijtijd;
-        public MatrixVak(string[] parts)
-        {
-            afstand = int.Parse(parts[2]);
-            rijtijd = int.Parse(parts[3]);
         }
     }
 }
